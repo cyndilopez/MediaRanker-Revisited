b4f81f5a2b1de64f2b292c779b237d17a65d0122
Cyndi Lopez <cyndilopez6@gmail.com>
2019-04-29 20:05:57 -0700

    user destroy method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,13 @@ class UsersController < ApplicationController
     redirect_to root_path
   end
 
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Successfully logged out!"
+
+    redirect_to root_path
+  end
+
   # def login_form
   # end
 
--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,6 @@ Rails.application.routes.draw do
   get "/auth/github", as: "github_login"
 
   get "/auth/:provider/callback", to: "users#create"
+
+  delete "/logout", to: "users#destroy", as: "logout"
 end
